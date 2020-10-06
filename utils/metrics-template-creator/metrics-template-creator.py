78100518295b7ce241f80843bfc265de6c7eacfd
Raghul Christus <raghul.christus@gmail.com>
2020-10-06 16:26:49 +0530

    Metrics template changes

--- utils/metrics-template-creator/metrics-template-creator.py
+++ utils/metrics-template-creator/metrics-template-creator.py
@@ -1,4 +1,5 @@
 import json
+import copy
 IN_JSON = "metrics.json"
 OUT_JSON = "out.json"
 
@@ -7,7 +8,7 @@ with open(IN_JSON) as f:
     metrics = json.load(f)
 f.close()
 
-unit_conversion = [
+UNIT_CONVERSION = [
     {"key": "_mbits_rate", "value": "Megabits/Second"},
     {"key": "_mbits", "value": "Megabytes"},
     {"key": "_mb", "value": "Megabytes"},
@@ -15,13 +16,16 @@ unit_conversion = [
     {"key": "percent", "value": "Percent"}
 ]
 
-counter_template = {
+COUNTER_TEMPLATE = {
     'MetricName': '',
     'Unit': 'Count',
     'Value': '',
+    'Timestamp': '',
     'Dimensions': [
-        {'Name': 'Description', 'Value': ''}
-        ]
+        {'Name': 'Description', 'Value': ''},
+        {'Name': 'CitrixADC-AutoScale-Group', 'Value': ''},
+        {'Name': 'CitrixADC-InstanceID', 'Value': ''}
+    ]
 }
 
 out_ds = dict()
@@ -29,27 +33,31 @@ for feature in metrics.keys():
     out_ds[feature] = dict()
     out_ds[feature]['counters'] = list()
     for cntr in metrics[feature].get('counters', []):
-        cntr_template = counter_template
-        cntr_template['MetricName'] = cntr[0]
-        cntr_template['Dimensions'][0]['Value'] = cntr[1]
-        #Find out the right Unit
-        for unit in unit_conversion:
-            if unit['key'] in cntr[1]:
+        cntr_template = copy.deepcopy(COUNTER_TEMPLATE) # Deep copy is required as we have dict() of dict()
+        metric_name = cntr[0]
+        metric_description = cntr[1]
+        cntr_template['MetricName'] = metric_name
+        cntr_template['Dimensions'][0]['Value'] = metric_description
+        #Find the right Unit
+        for unit in UNIT_CONVERSION:
+            if unit['key'] in metric_description:
                 cntr_template['Unit'] = unit['value']
                 break
-        out_ds[feature]['counters'].append(cntr_template.copy())
+        out_ds[feature]['counters'].append(cntr_template)
     for cntr in metrics[feature].get('gauges', []):
-        cntr_template = counter_template
-        cntr_template['MetricName'] = cntr[0]
-        cntr_template['Dimensions'][0]['Value'] = cntr[1]
-        #Find out the right Unit
-        for unit in unit_conversion:
-            if unit['key'] in cntr[1]:
+        cntr_template = copy.deepcopy(COUNTER_TEMPLATE) # Deep copy is required as we have dict() of dict()
+        metric_name = cntr[0]
+        metric_description = cntr[1]
+        cntr_template['MetricName'] = metric_name
+        cntr_template['Dimensions'][0]['Value'] = metric_description
+        #Find the right Unit
+        for unit in UNIT_CONVERSION:
+            if unit['key'] in metric_description:
                 cntr_template['Unit'] = unit['value']
                 break
-        out_ds[feature]['counters'].append(cntr_template.copy())
+        out_ds[feature]['counters'].append(cntr_template)
     
-#Read the input metrics json
+#Write to a JSON File
 with open(OUT_JSON, 'w') as f:
     json.dump(out_ds, f, indent=4)
 f.close()
